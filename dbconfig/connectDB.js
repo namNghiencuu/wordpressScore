5b6f93c593b5a8f81d5d6abd509194a4c2ee70eb
Nam <phanhoangn98@gmail.com>
2019-07-16 12:26:33 +0700

    update connect db

--- dbconfig/connectDB.js
+++ dbconfig/connectDB.js
@@ -3,7 +3,7 @@ const uri =
   "mongodb+srv://test:admin123@muahexanh-vwmur.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect((err, client) => {
-  if (error) throw error;
+  if (err) throw err;
   const collection = client.db("muahexanh").collection("expressscore");
   console.log("connected to database");
 });
